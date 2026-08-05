dd6f2b6613ab923514716e0962bdf1b9bfe2eefb
Nilton Dionisio Guerra <78931445+Niltonguerra@users.noreply.github.com>
2026-08-05 19:14:40 -0300

    Delete code/frontend/web/provider/teste123/dist directory

--- code/frontend/web/provider/teste123/dist/.dev-server/compiled-types/components/ProviderComponent.d.ts
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import './ProviderComponent.css';
-declare const ProviderComponent: React.FC;
-export default ProviderComponent;
--- code/frontend/web/provider/teste123/dist/@mf-types/compiled-types/components/MeuComponente.d.ts
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import './ProviderComponent.css';
-declare const MeuComponente: React.FC;
-export default MeuComponente;
